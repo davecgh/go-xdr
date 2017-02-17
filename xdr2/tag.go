4930550ba2e22f87187498acfd78348b15f4e7a8
Giovanni Bajo <rasky@develer.com>
2017-02-17 18:21:19 +0100

    Updated copyright

--- xdr2/tag.go
+++ xdr2/tag.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2014 Dave Collins <dave@davec.name>
+ * Copyright (c) 2015-2017 Giovanni Bajo <rasky@develer.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
